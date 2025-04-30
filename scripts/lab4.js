c0794e661a04403a039a29c7e1910263f04e8dd6
ChesterHuey <chhuey@ucsd.edu>
2025-04-29 22:52:04 -0700

    lab4 fixed

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -30,7 +30,7 @@ function discountPrices(prices, discount) {
     }
     const discounted = [];
     const length = prices.length;
-    let discountedPrice = 0
+    let discountedPrice = 0;
     for (let i = 0; i < length; i++) {
         discountedPrice = prices[i] * (1 - discount);
         discounted.push(prices[i] * (1 - discount));
